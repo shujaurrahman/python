dd57eb54c0bb537bda2643efd4c3c68291efc704
shujaurrahman <Shujaurrehman210@gmail.com>
2024-07-26 03:48:39 +0530

    Cwh python

---
+++ Cwh/4.py
@@ -0,0 +1,6 @@
+# pratice print 
+
+print(7*8)
+print("shujaurrahman")
+print(7)
+print(True)
---
+++ Cwh/5.py
@@ -0,0 +1,4 @@
+# comments and escape seq
+# \n for new line 
+
+print("shuja is my name \n I study at AMU. ")
